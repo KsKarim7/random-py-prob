a3223bfe4753d9b744ea415965ebe46ac88aa25c
Khaled Karim <kskarim177@gmail.com>
2023-03-26 15:16:26 +0600

    solved

--- prob8.py
+++ prob8.py
@@ -3,6 +3,8 @@
 class Anime:
     def __init__(self, title, arg=None):
         self.title = title
+        self.total_budget = 0
+        self.chr = {}
         if (arg == None):
             self.year = 2023
             self.season = 'Spring'
@@ -21,8 +23,35 @@ class Anime:
                     self.season = arg
 
     def animeInfo(self):
-        print(
-            f'Title: {self.title} \nYear: {self.year} \nSeason: {self.season}')
+        if (self.total_budget == 0):
+            print(
+                f'Title: {self.title} \nYear: {self.year} \nSeason: {self.season}')
+        else:
+            print(
+                f'Title: {self.title} \nYear: {self.year} \nSeason: {self.season} \nTotal Budget: {self.total_budget} \nCharacters: {self.chr}')
+
+    def buildCharacter(self, *args):
+        cost = {'Male': 400, 'Female': 500,
+                'Protag': 200, 'Antag': 170, 'Mob': 100}
+        self.count = len(args) // 2
+        sum = 0
+        print(f'{self.count} characters added to {self.title}')
+        for i in range(0, len(args), 2):
+            for j in args[i + 1]:
+                sum += cost[j]
+            if (len(args[i+1]) == 1):
+                sum += cost['Mob']
+            self.chr[args[i]] = sum
+            self.total_budget += sum
+            sum = 0
+
+    def compareBudget(self, obj):
+        if (self.total_budget > obj.total_budget):
+            return f'{self.title} has more budget than {obj.title}'
+        elif (self.total_budget < obj.total_budget):
+            return f'{self.title} has less budget than {obj.title}'
+        else:
+            return f'{self.title} and {obj.title} has equal budget'
 
 
 a1 = Anime('Oshi No Ko')
@@ -38,3 +67,25 @@ a3.animeInfo()
 print('4#####################')
 a4.animeInfo()
 print('5#####################')
+a1.buildCharacter('Hoshino', ['Female',
+                              'Protag'], 'Gorou', ['Male', 'Antag'],
+                  'Pieyon', ['Male'])
+print('---------------------')
+a1.animeInfo()
+print('6#####################')
+a2.buildCharacter('Kakushi', ['Male',
+                              'Protag'], 'Hime', ['Female',
+                                                  'Protag'])
+print('---------------------')
+a2.animeInfo()
+print('7#####################')
+a3.buildCharacter('Ferris', ['Female'])
+print('---------------------')
+a3.animeInfo()
+print('8#####################')
+a4.buildCharacter('Noe', ['Male', 'Protag'])
+print(f'Total Budget of {a4.title} is {a4.total_budget}')
+print('9#####################')
+print(a1.compareBudget(a2))
+print(a3.compareBudget(a2))
+print(a3.compareBudget(a4))
